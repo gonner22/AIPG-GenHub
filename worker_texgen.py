7532e55cefc6c60e355d9f445d1a40fc4c0c5f63
Topper <Topper@somewhere.net>
2024-03-23 17:22:41 -0400

    Update main script

--- worker_texgen.py
+++ worker_texgen.py
@@ -1,8 +1,11 @@
 import os
+import shutil
 import subprocess
 import yaml
 
 def clone_repo(repo_url, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
     subprocess.run(['git', 'clone', repo_url, destination])
 
 def build_docker_image():
@@ -14,23 +17,34 @@ def run_docker_container(exec_type, ports, network, container_name, image_name, 
     if exec_type:
         command.insert(2, '-'+exec_type)
 
-    for key, value in kwargs.items():
-        command.append('--'+key)
-        command.append(value)
+    # Check for Aphrodite-specific arguments
+    if 'gpus' in kwargs:
+        command.extend(['--gpus', kwargs['gpus']])
+    if 'shm-size' in kwargs:
+        command.extend(['--shm-size', kwargs['shm-size']])
+    if 'env' in kwargs:
+        for env_var in kwargs['env']:
+            command.extend(['-e', env_var])
 
     command.append(image_name)
-    
-    subprocess.run(command)
+
+    command_str = ' '.join(command)
+
+    print("Final Docker Command for Aphrodite:")
+    print(command_str)  # Print the final Docker command
+
+    subprocess.run(command_str, shell=True)
 
 if __name__ == "__main__":
     # ASCII Art
     print('''
-   _   _ _____ _     _____ ____  __  __ _____ 
-  / \ | | ____| |   | ____|  _ \|  \/  | ____|
- / _ \| |  _| | |   |  _| | |_) | |\/| |  _|  
-/ ___ \ | |___| |___| |___|  _ <| |  | | |___ 
-/_/   \_\_____|_____|_____|_| \_\_|  |_|_____|
-                                              
+           _____ _____   _____         _____            _    _       _     
+     /\   |_   _|  __ \ / ____|       / ____|          | |  | |     | |    
+    /  \    | | | |__) | |  __ ______| |  __  ___ _ __ | |__| |_   _| |__  
+   / /\ \   | | |  ___/| | |_ |______| | |_ |/ _ \ '_ \|  __  | | | | '_ \ 
+  / ____ \ _| |_| |    | |__| |      | |__| |  __/ | | | |  | | |_| | |_) |
+ /_/    \_\_____|_|     \_____|       \_____|\___|_| |_|_|  |_|\__,_|_.__/ 
+
     ''')
     print("Centralized repository for AI worker and generators")
     print("Location: United States of America / Web: aipowergrid.io / X: @AIPowerGrid / e-mail: admin@aipowergrid.io\n")
@@ -45,18 +59,28 @@ if __name__ == "__main__":
     clone_repo("https://github.com/gonner22/AI-Horde-Worker", "AI-Horde-Worker")
     clone_repo("https://github.com/gonner22/aphrodite-engine", "aphrodite-engine")
 
-    # Change directory to cloned repositories
-    os.chdir("AI-Horde-Worker")
-    # Build worker docker image
-    build_docker_image()
+    # Check if bridgeData.yaml exists
+    if not os.path.exists("bridgeData.yaml"):
+        print("Error: bridgeData.yaml not found!")
+        print("Please copy bridgeData_template.yaml file with the name bridgeData.yaml in the root directory including your configurations.")
+        exit(1)
 
-    os.chdir("../aphrodite-engine")
+    # Copy bridgeData.yaml to AI-Horde-Worker directory
+    shutil.copy("bridgeData.yaml", "AI-Horde-Worker")
+
+    # Change directory to AI-Horde-Worker
+    os.chdir("AI-Horde-Worker")
+
+    # Build worker docker image
+#    build_docker_image()
+
     # Run worker docker container
-    run_docker_container(**config['worker_config'])
+#    run_docker_container(**config['worker_config'])
 
     # Change directory back to the main directory
     os.chdir("..")
 
     os.chdir("aphrodite-engine")
     # Run aphrodite-engine docker container
+    print("Aphrodite Config:", config['aphrodite_config'])
     run_docker_container(**config['aphrodite_config'])
